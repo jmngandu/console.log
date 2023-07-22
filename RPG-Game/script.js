7afeb547649e5bfe7275adfdca4ba3a19313ef7c
James Ngandu <jamesmngandu@gmail.com>
2023-07-22 10:20:30 +0300

    added

--- RPG-Game/script.js
+++ RPG-Game/script.js
@@ -9,6 +9,9 @@ let inventory =["stick"];
 const button1 = document.querySelector("#button1");
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
-
+const text = document.querySelector("#text");
+const xpText = document.querySelector("#xpText");
+const healthText = document.querySelector("#healthText");
+const goldText = document.querySelector("#goldText")
 
 
