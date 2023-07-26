757b96848267f6ab572134e93820e80075b3fce2
James Ngandu <jamesmngandu@gmail.com>
2023-07-26 23:10:13 +0300

    added

---
+++ calculator2/script.js
@@ -0,0 +1,15 @@
+let chartg = "f";
+const timesn = 5;
+let ans = ''
+
+for(let i = 0; i < timesn; i++){
+    ans = ans + chartg
+    
+} 
+console.log(ans);
+
+function addy(num1, num2){
+    return num1 + num2;
+}
+
+console.log(addy(5, 6))
