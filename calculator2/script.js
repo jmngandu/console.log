1e0c8c175054bac3b3eff715fdd5f1e0defead63
James Ngandu <jamesmngandu@gmail.com>
2023-08-02 16:38:42 +0300

    calculator

--- calculator2/script.js
+++ calculator2/script.js
@@ -2,7 +2,12 @@ let runningTotal = 0;
 let buffer = "0";
 let previousOperator = 0;
 
-const screen = document.querySelector(".screen");
+const screen = document.querySelector('.screen');
 
 function buttonClick (){ }
 
+function init () {
+
+}
+
+init();
