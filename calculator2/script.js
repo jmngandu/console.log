b5c7c80bcd71b374d37f22b30d45f74f87490ea5
James Ngandu <jamesmngandu@gmail.com>
2023-08-02 16:41:21 +0300

    calculator

--- calculator2/script.js
+++ calculator2/script.js
@@ -7,6 +7,8 @@ const screen = document.querySelector('.screen');
 function buttonClick (){ }
 
 function init () {
+    document.querySelector('.calc-buttons')
+    .addEventListener("click", function())
 
 }
 
