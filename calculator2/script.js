793a64b5c3974be79a3efd5ccaa1b321f4e6f518
James Ngandu <jamesmngandu@gmail.com>
2023-08-02 17:11:11 +0300

    calculator

--- calculator2/script.js
+++ calculator2/script.js
@@ -5,11 +5,17 @@ let previousOperator = 0;
 const screen = document.querySelector('.screen');
 
 function buttonClick (value){
-    console.log(value);
+    if(isNaN(value)){
+        //this is not a number
+        handleSymbol(value);
+    }else {
+        //this is a number
+        handleNumber(value);
+    }
  }
 
- function handleSymbol(){}
- function handleNumber(){}
+ function handleSymbol(symbol){}
+ function handleNumber(numberString){}
 
 function init () {
     document.querySelector('.calc-buttons')
