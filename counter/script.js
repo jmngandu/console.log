5c532af4275ca31ced5cfb0f6c9561c8ec2df43a
James Ngandu <jamesmngandu@gmail.com>
2023-08-04 22:48:02 +0300

    added

---
+++ counter/script.js
@@ -0,0 +1,8 @@
+const county = document.querySelector('#increment-btn');
+const numbr = document.querySelector('#count-display');
+let currentCount = 0;
+county.addEventListener('click', function(){
+    numbr.innerText = "1"
+    
+
+})
