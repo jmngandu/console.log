9c112a24e5b64810e1c947bcdf46cec275c8a90c
James Ngandu <jamesmngandu@gmail.com>
2023-08-11 17:56:35 +0300

    added

--- zlearn/nested=loops/nest.js
+++ zlearn/nested=loops/nest.js
@@ -4,12 +4,10 @@ let numberGrid = [
     [4, 5, 6],
     [7, 8, 9]
 ]
-for(let i=0; i < numberGrid.length; i++){
-    for(let j=0; j < numberGrid[i].length; j++){
-        document.write(`${numberGrid[i][j]} <br/>`);
+for(let i = 0; i < numberGrid.length; i++){
+    for(let j = 0; j < numberGrid[i].length; j++){
+        document.write(numberGrid[i][j]}"<br/>");
     }
 }
 
 
-
-document.write(numberGrid[2][0]);//[row] [colos]
