bdf4df71f1631fc1c7890ffa301d452c4b868a83
James Ngandu <jamesmngandu@gmail.com>
2023-08-11 17:21:52 +0300

    added

--- password-checker/pass.js
+++ password-checker/pass.js
@@ -4,13 +4,17 @@ var entryCount = 0;
 var limit = 3;
 var error = false;
 
-while(trial != password){
+while(trial != password && !error){
     if(entryCount < limit){
-        trial = window.prompt("Enter password")
+        trial = window.prompt("Enter password");
         entryCount++;
     }else{
         error = true;
+    }}
+
+    if(error){
+        alert("Too many entries");
+    }else{
+        alert("You entered correct password")
     }
-    
-}
-alert("You entered correct password")
+
