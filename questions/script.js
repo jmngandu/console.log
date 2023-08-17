d28b16d9e7c42c67dea3928d547e73a14b38d885
James Ngandu <jamesmngandu@gmail.com>
2023-08-17 21:50:27 +0300

    added

---
+++ questions/script.js
@@ -0,0 +1,18 @@
+let questions = [
+    {
+        prompt: "Where is Kenya located?\n (a) East Africa\n
+        (b) West Africa",
+        answer: "a"
+    },
+    {
+        prompt: "Which is the largest city in kenya?\n (a)Mombasa  \n
+        (b) Nairobi" \n (c)Kisumu ,
+        answer: "b"
+    },
+    {
+        prompt: "Is Kitui a county or a country?\n (a) country \n
+        (b) county",
+        answer: "b"
+    }
+    ]
+    
