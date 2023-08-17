53b909b25e894bb03c285add164b2ec5a3027f44
James Ngandu <jamesmngandu@gmail.com>
2023-08-17 21:18:51 +0300

    added

--- save/script.js
+++ save/script.js
@@ -2,6 +2,5 @@ const textbox = document.getElementById("textbox");
 const button = document.getElementById("btn");
 
 button.addEventListener("click", function(){
-    const update = document.createElement("h1");
-    textbox.innerHTML = update
+   localStorage.name = textbox.ariaValueMax;
 })
