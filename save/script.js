be49c16d4e9d5e0a53058b82d8fb34f83c1508a4
James Ngandu <jamesmngandu@gmail.com>
2023-08-17 21:15:43 +0300

    added

--- save/script.js
+++ save/script.js
@@ -1,2 +1,7 @@
 const textbox = document.getElementById("textbox");
 const button = document.getElementById("btn");
+
+button.addEventListener("click", function(){
+    const update = document.createElement("h1");
+    textbox.innerHTML = update
+})
