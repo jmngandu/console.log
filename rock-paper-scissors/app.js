5e0c19ef11622aadd3eac01351ff985f9df6c27e
James Ngandu <jamesmngandu@gmail.com>
2023-08-20 09:46:59 +0300

    added

--- rock-paper-scissors/app.js
+++ rock-paper-scissors/app.js
@@ -4,6 +4,7 @@ const resultDisplay = document.getElementById("result");
 const possibleChoices = document.querySelectorAll("button");
 
 let userchoice
+let computeChoice
 possibleChoices.forEach(possibleChoice =>possibleChoice.addEventListener("click", (e) => {
     userchoice = e.target.id
     UserDisplayChoice.innerHTML = userchoice;
@@ -11,7 +12,18 @@ possibleChoices.forEach(possibleChoice =>possibleChoice.addEventListener("click"
 }))
 
 function generateComputerChooice() {
-    const randomNumber = Math.floor(Math.random() * 3)
+    const randomNumber = Math.floor(Math.random() * 3) + 1
+    
+    if(randomNumber === 1){
+        computeChoice = "Rock"
+    }
+    if(randomNumber === 2){
+        computeChoice = "Paper"
+    }
+    if(randomNumber === 3){
+        computeChoice = "Scissors"
+    }
+    ComputerDisplayChoice.innerHTML = computeChoice
 }
 
 
