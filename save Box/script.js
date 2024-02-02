2cbb4922790cabfd325abfb492df744086390e26
jmngandu <jamesmngandu@gmail.com>
2024-02-02 17:35:00 +0300

    commit

--- save Box/script.js
+++
@@ -1,7 +0,0 @@
-const textbox = document.getElementById("textbox");
-const button = document.getElementById("btn");
-
-button.addEventListener("click", function(){
-   localStorage.name = textbox.value;
-});
-document.write("<br>", localStorage.name);
